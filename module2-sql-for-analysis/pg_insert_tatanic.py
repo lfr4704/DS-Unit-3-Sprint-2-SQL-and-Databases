77fa1bf4b0c6957ea5d3b04eed9d080dcdb270e7
Luis Robles <lfr4704@gmail.com>
2020-05-26 19:02:13 -0700

    revision to pg titanic py

--- module2-sql-for-analysis/pg_insert_tatanic.py
+++ module2-sql-for-analysis/pg_insert_tatanic.py
@@ -1,8 +1,13 @@
 import os
 from dotenv import load_dotenv
+import pandas as pd
 import psycopg2
+from psycopg2.extras import execute_values
+import numpy as np
 
 load_dotenv() #> loads contents of the .env file into the script's environment
+
+psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
 
 #read passenger data from the csv file
 csv_path = os.path.join(os.path.dirname(__file__), "..", "module2-sql-for-analysis", "titanic.csv")
@@ -22,7 +27,7 @@ print("CONNECTION:", connection)
 cursor = connection.cursor()
 print("CURSOR:", cursor)
 
-# create a table to store the passengers
+# create a table to store the passenger
 query = """
 CREATE TABLE IF NOT EXISTS passengers (
     id SERIAL PRIMARY KEY,
@@ -31,8 +36,8 @@ CREATE TABLE IF NOT EXISTS passengers (
     name varchar,
     sex varchar,
     age int,
-    sib_spouse_count int,
-    parent_child_count int,
+    sib_spouse_count int4,
+    parent_child_count int4,
     fare float8
 );
 """
@@ -42,3 +47,8 @@ cursor.execute(query)
 connection.commit() # actually update the database
 
 # TODO: read CSV contents and insert rows into a new table
+
+list_of_tuples= list(df.to_records(index=False))
+
+insertion_query = f"INSERT INTO passengers (survived, pclass, name, ses, age, sib_spouse_count, parent_child_count, fare) VALUES %s"
+execute_values(cursor, insertion_query, list_of_tuples) #third param: data as a list
